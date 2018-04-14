6a49f6e8dc4cc29bd85df82cf4eaf65176d33b72
jose2007kj <jose2007kj@gmail.com>
2018-04-14 09:28:05 +0530

    added travel dicts,ontology,etc

--- travel_kb.py
+++ travel_kb.py
@@ -9,7 +9,7 @@ import re
 # import numpy as np
 # import pandas as pd
 # from pandas import DataFrame, read_csv
-with open('only_iob_kj_donbosco.iob', 'w') as f:
+with open('trash.iob', 'w') as f:
 	with open('frames.json', 'r') as file_object:
 		contents = json.load(file_object)
 		crf = []
@@ -17,14 +17,57 @@ with open('only_iob_kj_donbosco.iob', 'w') as f:
 		num = 0
 		no_slots = []
 		ontology = {}
+		travel_dict = {}
 		onto_key = 0
 		boolean = []
 		text = []
+		travel_key = 0
 		# df=pd.DataFrame(columns=['nl', 'act1', 'act2', 'act3', 'act4', 'slot1', 'slot2', 'slot3', 'slot4'])
 		for i in range(0,1350):
 			print(i, '****************************')
 			for item in contents[i]['turns']:
 				if 'user' in item['author']:
+					if travel_key not in travel_dict:
+						travel_dict[travel_key] = {'palace':'','seat':'','duration':'','str_date':'','cathedral':'','gst_rating':'','spa':'','category':''
+,'museum':''
+,'or_city':''
+,'beach':''
+,'end_date':''
+,'ref_anaphora':''
+,'price':''
+,'park':''
+,'downtown':''
+,'dst_city':''
+,'n_adults':''
+,'name':''
+,'university':''
+,'dep_time_or':''
+,'airport':''
+,'amenities':''
+,'wifi':''
+,'budget':''
+,'parking':''
+,'breakfast':''
+,'count_name':''
+,'count':''
+,'n_children':''
+,'count_dst_city':''
+,'count_seat':''
+,'flex':''
+,'vicinity':''
+,'dep_time_dst':''
+,'gym':''
+,'shopping':''
+,'mall':''
+,'max_duration':''
+,'market':''
+,'arr_time_or':''
+,'min_duration':''
+,'thankyou':''
+,'goodbye':''
+,'affirm':''
+,'greeting':''
+,'negate':''}
 					#tokens = nltk.word_tokenize(item['text']) 
 					#tokens = wordpunct_tokenize(item['text'])
 					#print(tokens)
@@ -110,6 +153,12 @@ with open('only_iob_kj_donbosco.iob', 'w') as f:
 												iob[pos-1] = iob[pos-1].replace("O", tag)
 												tag = "I_"+args['name']+"_"+val['key']
 												iob[pos] = iob[pos].replace("O", tag)
+												# if val[key] not in ontology:
+												# 	ontology[args['name']] = {}
+												travel_temp={val['key']:'no flexible'}
+												travel_dict[travel_key].update(travel_temp)
+												# travel_dict[travel_key][val['key']]= 'no flexible'
+												#travel_key =travel_key+1
 												if args['name'] not in ontology:
 													ontology[args['name']] = {}
 												if val['key'] not in ontology[args['name']]:
@@ -132,6 +181,11 @@ with open('only_iob_kj_donbosco.iob', 'w') as f:
 												for onto_iter in range(len(ontology[args['name']][val['key']]),len(ontology[args['name']][val['key']])+1):
 													if "no"+word not in ontology[args['name']][val['key']].values():
 														ontology[args['name']][val['key']][onto_iter] = "no"+word
+												# travel_dict[travel_key][val['key']]= 'noflexible'
+												travel_temp={val['key']:'noflexible'}
+												# travel_dict
+												travel_dict[travel_key].update(travel_temp)
+												#travel_key =travel_key+1
 												# onto_key = onto_key+1
 											elif "not"+word in final:
 												# final = final.replace("not"+word,'$'+val['key']+'$')
@@ -145,6 +199,10 @@ with open('only_iob_kj_donbosco.iob', 'w') as f:
 												for onto_iter in range(len(ontology[args['name']][val['key']]),len(ontology[args['name']][val['key']])+1):
 													if "not"+word not in ontology[args['name']][val['key']]:
 														ontology[args['name']][val['key']][onto_iter] = "not"+word
+												# travel_dict[travel_key][val['key']]= 'notflexible'
+												travel_temp={val['key']:'notflexible'}
+												travel_dict[travel_key].update(travel_temp)
+												#travel_key =travel_key+1
 												# onto_key = onto_key+1
 											elif "not "+word in final:
 												# final = final.replace("not "+word,'$'+val['key']+'$')
@@ -160,6 +218,10 @@ with open('only_iob_kj_donbosco.iob', 'w') as f:
 												for onto_iter in range(len(ontology[args['name']][val['key']]),len(ontology[args['name']][val['key']])+1):
 													if "not "+word not in ontology[args['name']][val['key']].values():
 														ontology[args['name']][val['key']][onto_iter] = "not "+word
+												# travel_dict[travel_key][val['key']]= 'not flexible'
+												travel_temp={val['key']:'not flexible'}
+												travel_dict[travel_key].update(travel_temp)
+												#travel_key =travel_key+1
 												# onto_key = onto_key+1
 											else:								
 												# final = final.replace(word,'$'+val['key']+'$')
@@ -173,6 +235,10 @@ with open('only_iob_kj_donbosco.iob', 'w') as f:
 												for onto_iter in range(len(ontology[args['name']][val['key']]),len(ontology[args['name']][val['key']])+1):
 													if word not in ontology[args['name']][val['key']].values():	
 														ontology[args['name']][val['key']][onto_iter] = word
+												# travel_dict[travel_key][val['key']]= 'flexible'
+												travel_temp={val['key']:'flexible'}
+												travel_dict[travel_key].update(travel_temp)
+												#travel_key =travel_key+1
 												# onto_key = onto_key+1
 										# elif "wifi" in word:
 										# 	if "free "+word in final:
@@ -200,6 +266,10 @@ with open('only_iob_kj_donbosco.iob', 'w') as f:
 												for onto_iter in range(len(ontology[args['name']][val['key']]),len(ontology[args['name']][val['key']])+1):
 													if "free "+word not in ontology[args['name']][val['key']].values():
 														ontology[args['name']][val['key']][onto_iter] = "free "+word
+												# travel_dict[travel_key][val['key']]= 'free '+word
+												travel_temp={val['key']:'free '+word}
+												travel_dict[travel_key].update(travel_temp)
+												#travel_key =travel_key+1
 												# onto_key = onto_key+1
 												print("no '''''")
 											elif "free"+word in final:
@@ -214,6 +284,10 @@ with open('only_iob_kj_donbosco.iob', 'w') as f:
 												for onto_iter in range(len(ontology[args['name']][val['key']]),len(ontology[args['name']][val['key']])+1):
 													if "free"+word not in ontology[args['name']][val['key']].values():
 														ontology[args['name']][val['key']][onto_iter] = "free"+word
+												# travel_dict[travel_key][val['key']]= 'free'+word
+												travel_temp={val['key']:'free'+word}
+												travel_dict[travel_key].update(travel_temp)
+												#travel_key =travel_key+1
 												# onto_key = onto_key+1
 											else:	
 												tag = "B_"+args['name']+"_"+val['key']
@@ -227,6 +301,10 @@ with open('only_iob_kj_donbosco.iob', 'w') as f:
 												for onto_iter in range(len(ontology[args['name']][val['key']]),len(ontology[args['name']][val['key']])+1):
 													if word not in ontology[args['name']][val['key']].values():
 														ontology[args['name']][val['key']][onto_iter] = word
+												# travel_dict[travel_key][val['key']]= word
+												travel_temp={val['key']:word}
+												travel_dict[travel_key].update(travel_temp)
+												#travel_key =travel_key+1
 												# onto_key = onto_key+1
 										# else:			
 										# 	final = final.replace(word,'$'+val['key']+'$')
@@ -286,6 +364,10 @@ with open('only_iob_kj_donbosco.iob', 'w') as f:
 								# print(res)
 								tag = "B_"+args['name']+"_"+val['key']
 								iob[pos] = iob[pos].replace("O", tag)
+								# travel_dict[travel_key][val['key']]= n_val
+								travel_temp={val['key']:n_val}
+								travel_dict[travel_key].update(travel_temp)
+								#travel_key =travel_key+1
 								# for x in range(0,len(iob)): #working for loop
 								# 	iob[x] = iob[x].replace("O", "nmnmnm")
 								# 	print("iob",iob[x])
@@ -297,7 +379,10 @@ with open('only_iob_kj_donbosco.iob', 'w') as f:
 
 										if word in word2:
 											print(word,idx,idy)
-
+											# travel_dict[travel_key][val['key']]= n_val
+											travel_temp={val['key']:n_val}
+											travel_dict[travel_key].update(travel_temp)
+											#travel_key =travel_key+1
 											pos = idy
 											# print("position brek",pos)
 											if idx == 0:
@@ -390,17 +475,45 @@ with open('only_iob_kj_donbosco.iob', 'w') as f:
 										# if k == 0:
 										iob[l]="B_greeting"
 										iob[l+1]=" I_greeting"
+										# travel_dict[travel_key]['greeting']= 'good '+final[l+1]
+										travel_temp={'greeting':'good '+final[l+1]}
+										travel_dict[travel_key].update(travel_temp)
+										#travel_key =travel_key+1
 								elif m in greeting:
 									iob[l] = "B_greeting"
+									# travel_dict[travel_key]['greeting']= m
+									travel_temp={'greeting':m}
+									travel_dict[travel_key].update(travel_temp)
+									#travel_key =travel_key+1
 								elif m in moreinfo:
+									# travel_dict[travel_key]['moreinfo']= m
+									travel_temp={'moreinfo':m}
+									travel_dict[travel_key].update(travel_temp)
+									#travel_key =travel_key+1
 									iob[l] = 'B_moreinfo'
 								elif m in affirm:
+									# travel_dict[travel_key]['affirm']= m
+									travel_temp={'affirm':m}
+									travel_dict[travel_key].update(travel_temp)
+									#travel_key =travel_key+1
 									iob[l] = 'B_affirm'
 								elif "thank" in m:
+									# travel_dict[travel_key]['thankyou']= m
+									travel_temp={'thankyou':m}
+									travel_dict[travel_key].update(travel_temp)
+									#travel_key =travel_key+1
 									iob[l] = "B_thankyou"
 								elif 'bye' in m:
+									# travel_dict[travel_key]['goodbyye']= m
+									travel_temp={'goodbye':m}
+									travel_dict[travel_key].update(travel_temp)
+									#travel_key =travel_key+1
 									iob[l] = 'B_goodbye'
 								elif 'no' == m or 'nope' == m:
+									# travel_dict[travel_key]['negate']= m
+									travel_temp={'negate':m}
+									travel_dict[travel_key].update(travel_temp)
+									#travel_key =travel_key+1
 									iob[l] = 'B_negate'
 								else:
 									print("b")
@@ -410,17 +523,38 @@ with open('only_iob_kj_donbosco.iob', 'w') as f:
 										# if k == 0:
 										iob[l]=" B_greeting"
 										iob[l+1]=" I_greeting"
+										travel_temp={'greeting':'good '+final[l+1]}
+										travel_dict[travel_key].update(travel_temp)
+										#travel_key =travel_key+1
 								elif m in greeting:
 									iob[l] = " B_greeting"
+									travel_temp={'greeting':m}
+									travel_dict[travel_key].update(travel_temp)
+									#travel_key =travel_key+1
 								elif m in moreinfo:
 									iob[l] = ' B_moreinfo'
+									travel_temp={'moreinfo':m}
+									travel_dict[travel_key].update(travel_temp)
+									#travel_key =travel_key+1
 								elif m in affirm:
 									iob[l] = ' B_affirm'
+									travel_temp={'affirm':m}
+									travel_dict[travel_key].update(travel_temp)
+									#travel_key =travel_key+1
 								elif "thank" in m:
 									iob[l] = " B_thankyou"
+									travel_temp={'thankyou':m}
+									travel_dict[travel_key].update(travel_temp)
+									#travel_key =travel_key+1
 								elif 'bye' in m:
 									iob[l] = ' B_goodbye'
+									travel_temp={'goodbye':m}
+									travel_dict[travel_key].update(travel_temp)
+									#travel_key =travel_key+1
 								elif 'no' == m or 'nope' == m:
+									travel_temp={'negate':m}
+									travel_dict[travel_key].update(travel_temp)
+									#travel_key =travel_key+1
 									iob[l] = ' B_negate'
 								else:
 									print("b")
@@ -463,6 +597,8 @@ with open('only_iob_kj_donbosco.iob', 'w') as f:
 					# 		for intent in act['info']['intent']:
 					# 			print(intent['val'])
 					# print(df)
+			travel_key =travel_key+1
+
 		# df.to_csv('iob crf_new2.csv', index=False)     
 		# print("boolean",boolean ,text)
 		# with open('unused_acts', 'w') as outfile: #by jose
@@ -470,3 +606,62 @@ with open('only_iob_kj_donbosco.iob', 'w') as f:
 		# with open('iob ontology2.json', 'w') as outfile: #by jose
 		# 	json.dump(ontology, outfile)
 		# f.write(''.join(set(no_slots)))	
+
+		# 			ll = 0
+		# 			greeting = ['hi','hello','heyo','good morning','hey','greetings']
+		# 			moreinfo = ['more','details']
+		# 			affirm = ['ok','ya','yeah','please do','yes']
+		# 			if len(iob) == iob_count:
+		# 				print("^^^^^^^^#########################################################^^^^^^^^^^",iob,intent)
+		# 				intentlist = intent.split()
+		# 				# intentlist= [i for i in words if i.lower() not in stopwords]
+		# 				for i, w in enumerate(intentlist):
+		# 					if w == 'confirm'
+		# 						intentlist[i] ='affirm'
+
+		# 				ioblen = len(iob)
+		# 				for k in range(0,len(intentlist)): #working code for inserting intenbt
+		# 					if k ==0:
+		# 						iob[k+1]=intentlist[k]
+		# 					else:
+		# 						iob[k+1]= " "+intentlist[k]  #end here
+		# 					if intentlist[k] == 'greeting':
+
+
+						
+		# 			f.write(''.join(iob))
+		# 			f.write("\n")
+		# 			# f.write(intent)
+		# 			# f.write("\n")
+		# 				# print("@@@@@@@@@@@@@@@@",iob)
+		# 			for k in range(len(act), 4):
+		# 				act.append('xxx')
+		# 			for l in range(len(slots), 4):
+		# 				slots.append('xxx')
+		# 			print("- - - - - - - - - - - - - - - - - - - - -- - - - - -")
+		# 			print(final,act,slots)
+		# 			if 'xxx' not in act[0]:
+		# 				# df.loc[num] = pd.Series(dict(nl=final, act1=act[0], act2=act[1], act3=act[2], act4=act[3], slot1= slots[0], slot2= slots[1], slot3= slots[2], slot4= slots[3]))
+		# 				num=num+1
+		# 			# jk = [[final,act[0],act[1],act[2],act[3],slots[0],slots[1],slots[2],slots[3]]]
+		# 			# kj.append(final,act[0],act[1],act[2],act[3],slots[0],slots[1],slots[2],slots[3])
+		# 			# kj.append(jk)
+		# 			# print(kj)
+		# 			# df.append({'nl': final, 'act1':act[0], 'act2':act[1], 'act3':act[2], 'slot1':slots[0], 'slot2':slots[1], 'slot3':slots[2]}, ignore_index=True)
+		# 			# crf.append(list(zip(final,act[0],act[1],act[2],act[3],slots[0],slots[1],slots[2],slots[3])))
+		# 			# print(crf)
+		# 			# df = pd.DataFrame(jk, columns=['nl','act1','act2','act3','act4','slot1','slot2','slot3','slot4'])
+					
+				
+		# 			# for act in item['labels']['frames']:
+		# 			# 	if 'intent' in act['info']:
+		# 			# 		for intent in act['info']['intent']:
+		# 			# 			print(intent['val'])
+		# 			# print(df)
+		# # df.to_csv('iob crf_new2.csv', index=False)     
+		# # print("boolean",boolean ,text)
+		# # with open('unused_acts', 'w') as outfile: #by jose
+		# # 	json.dump(set(no_slots),outfile)
+		with open('travel_dict.json', 'w') as outfile: #by jose
+			json.dump(travel_dict, outfile)
+		# # f.write(''.join(set(no_slots)))	
